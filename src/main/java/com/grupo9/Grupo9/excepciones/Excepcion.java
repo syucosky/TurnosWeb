43bdab996cf2204094c4941eab75340ea02af10a
mauroezeflores <mauroezequielflores48@gmail.com>
2023-05-11 20:09:49 -0300

    2 - clase agregada en cada package

---
+++ src/main/java/com/grupo9/Grupo9/excepciones/Excepcion.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.grupo9.Grupo9.excepciones;
+
+/**
+ *
+ * @author Dell
+ */
+public class Excepcion {
+    
+}
