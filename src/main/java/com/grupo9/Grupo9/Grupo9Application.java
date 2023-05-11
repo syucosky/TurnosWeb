43e40c0e6d53a7f459923ff49ae8c98a4d4cc107
mauroezeflores <mauroezequielflores48@gmail.com>
2023-05-11 19:58:05 -0300

    1 - subiendo proyecto

---
+++ src/main/java/com/grupo9/Grupo9/Grupo9Application.java
@@ -0,0 +1,13 @@
+package com.grupo9.Grupo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo9/Grupo9/Grupo9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo9.Grupo9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
