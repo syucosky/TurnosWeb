8797dbf293b5bc9b94cf1d1a92d8136c36f5ad23
syucosky <syucosky@gmail.com>
2023-05-15 20:22:23 -0300

    modificaciones parte del front

--- src/main/java/com/grupo9/Grupo9/controller/IndexController.java
+++ src/main/java/com/grupo9/Grupo9/controller/IndexController.java
@@ -2,16 +2,19 @@
 package com.grupo9.Grupo9.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
     
-    @GetMapping("index")
-    public String vista(){    
+    @GetMapping("/")
+    public String vista(Model model){    
         return "index.html";
     }
 }
