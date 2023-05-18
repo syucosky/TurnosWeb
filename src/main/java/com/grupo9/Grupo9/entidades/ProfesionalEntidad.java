3167fd4539e3ab5d2f07c6e3b2ad60607db37e0b
YamiRigo <92380361+YamiRigo@users.noreply.github.com>
2023-05-18 13:49:30 -0300

    Update ProfesionalEntidad.java

--- src/main/java/com/grupo9/Grupo9/entidades/ProfesionalEntidad.java
+++ src/main/java/com/grupo9/Grupo9/entidades/ProfesionalEntidad.java
@@ -1,6 +1,5 @@
 package com.grupo9.Grupo9.entidades;
 
-import com.grupo9.Grupo9.repositorios.ProfesionalRepository;
 import java.io.Serializable;
 import java.util.List;
 import javax.persistence.CascadeType;
@@ -11,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,9 +25,17 @@ public class ProfesionalEntidad implements Serializable{
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int dni;
     private String nombre;
-    private String especialidad;
+    private String email;
+    private String password;
+    private EspecialidadEntidad especialidad;
     private String tipoAtencion;
     private String ubicacion;
+    
+    private Boolean alta;
+    
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private RolEntidad rol;
     
     @ManyToMany(
             cascade = {
@@ -45,4 +53,24 @@ public class ProfesionalEntidad implements Serializable{
     private int puntosRecibidos; // SUMA DE LOS PUNTOS RECIBIDOS
     private int cantidadDeCalificaciones; // SUMA DE CADA PACIENTE QUE VOTO
     private int calificacion;  // PUNTAJE FINAL DEL PROFESIONAL
+
+    public ProfesionalEntidad() {
+    }
+
+    public ProfesionalEntidad(int dni, String nombre, String email, String password, EspecialidadEntidad especialidad, String tipoAtencion, String ubicacion, RolEntidad rol, List<ObraSocialEntidad> obraSocial, int puntosRecibidos, int cantidadDeCalificaciones, int calificacion) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.email = email;
+        this.password = password;
+        this.especialidad = especialidad;
+        this.tipoAtencion = tipoAtencion;
+        this.ubicacion = ubicacion;
+        this.rol = rol;
+        this.obraSocial = obraSocial;
+        this.puntosRecibidos = puntosRecibidos;
+        this.cantidadDeCalificaciones = cantidadDeCalificaciones;
+        this.calificacion = calificacion;
+    }
+    
 }
+
