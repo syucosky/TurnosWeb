ea40bd33abde67d62dd3f0f18742f4b005c9331f
YamiRigo <92380361+YamiRigo@users.noreply.github.com>
2023-05-18 11:48:07 -0300

    Entidades agregadas

--- src/main/java/com/grupo9/Grupo9/entidades/TurnosEntidad.java
+++ src/main/java/com/grupo9/Grupo9/entidades/TurnosEntidad.java
@@ -1,27 +1,57 @@
 package com.grupo9.Grupo9.entidades;
 
 
-import java.io.Serializable;
-import java.util.Date;
+//import java.io.Serializable;
+//import java.util.Date;
+//import javax.persistence.Entity;
+//import javax.persistence.Id;
+//import javax.persistence.OneToOne;
+//import javax.persistence.Table;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "turno")
-public class TurnosEntidad implements Serializable{
-    
+public class TurnosEntidad{
     @Id
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     
-    private String dia;
-    private Date horario; // FALTA DEFINIR EL TIPO DATE PARA SABER COMO MANEJAR LOS HORARIOS 
+    @Column(nullable = false) //la base de datos no puede contener valores nulos
+    private LocalDate fecha;
     
-    @OneToOne
-    private ProfesionalEntidad profesional;
+    @Column(nullable = false)
+    private LocalTime hora;
+    
+    @Column(nullable = false)
+    private String medico;
+    
+    @Column(nullable = false)
+    private String lugar;
+    
+    @Column(nullable = false)
+    private Boolean alta;
+    
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private EspecialidadEntidad especialidad;
+    
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private PacienteEntidad paciente;
+
+    public TurnosEntidad() {
+    }
+
+    
 }
