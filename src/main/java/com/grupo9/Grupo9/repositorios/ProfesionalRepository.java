17b66744c020072f9b21e2bbd169c7a2cec9c101
YamiRigo <92380361+YamiRigo@users.noreply.github.com>
2023-05-18 21:21:57 -0300

    Update ProfesionalRepository.java

--- src/main/java/com/grupo9/Grupo9/repositorios/ProfesionalRepository.java
+++ src/main/java/com/grupo9/Grupo9/repositorios/ProfesionalRepository.java
@@ -1,11 +1,21 @@
 package com.grupo9.Grupo9.repositorios;
 
 import com.grupo9.Grupo9.entidades.ProfesionalEntidad;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface ProfesionalRepository extends JpaRepository<ProfesionalEntidad, Long>{
-    
+public interface ProfesionalRepository extends JpaRepository<ProfesionalEntidad, Integer>{
+    Optional<ProfesionalEntidad> findByCorreo(String email);
+
+    boolean existsProfesionalByEmail(String email);
+
+    @Modifying
+    @Query("UPDATE Profesional a SET a.alta = :alta WHERE a.id = :id")
+    void baja(@Param("id") Integer id, @Param("alta") Boolean alta);
 } 
